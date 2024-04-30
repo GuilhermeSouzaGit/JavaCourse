8b64a730cca9930d8f4c7f20ee0dcebf86d29928
Guilherme <guisou1902@gmail.com>
2024-04-30 00:06:32 -0300

    feat: Tipos primitivos parte 5, exercicio

---
+++ src/aula_014_TiposPrimitivos_pt_5_exercicio/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,18 @@
+package aula_014_TiposPrimitivos_pt_5_exercicio;
+/*
+* Exercício
+*
+* Crie variáveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+*
+* Eu <nome>, morando no endereço <endereço>, confirmo que recebi o salário de <salário>, na data <data>
+* */
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String name = "Guilherme";
+        String address = "Rua xxxxxxxxxxxxxxxxxxxxx n° xx";
+        double salary = 1867.54;
+        String date = "29/04/2024";
+
+        System.out.println("Eu " + name + ", morando no endereço " + address + ", confirmo que recebi o salário de " + salary + ", na data " + date);
+    }
+}
