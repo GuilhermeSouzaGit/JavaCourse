71c95e7a41f35317cf74ac70eeaa8331d4a760aa
Guilherme <guisou1902@gmail.com>
2024-05-02 14:56:40 -0300

    feat: Arrays parte 2

---
+++ src/aula_033_Arrays02/Arrays02.md
@@ -0,0 +1,5 @@
+# Arrays parte 2
+## Padrões de inicialização de Arrays
+para os tipos `byte, short, int, long, float e double` o valor padrão que será inicializado será `0`,
+para `char` o valor será um unicode de espaço branco que é `\u0000` ou ` `, para tipos `boolean`por padrão será `false`
+e para `Strings` será `null`
---
+++ src/aula_033_Arrays02/Aula33Arrays.java
@@ -0,0 +1,14 @@
+package aula_033_Arrays02;
+
+public class Aula33Arrays {
+    public static void main(String[] args) {
+        // byte, short, int, long, float e double = 0
+        // char = "\u0000" = " "
+        // boolean = false
+        // String = null
+        String[] nomes = new String[3];
+        System.out.println(nomes[0]);
+        System.out.println(nomes[1]);
+        System.out.println(nomes[2]);
+    }
+}
