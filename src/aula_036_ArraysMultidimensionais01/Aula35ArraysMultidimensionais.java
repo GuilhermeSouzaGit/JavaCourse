559e86dd00cf9739592f007f2b90bc0032e0ee3d
Guilherme <guisou1902@gmail.com>
2024-05-02 16:36:21 -0300

    fix: changed Arrays numbers to match with the images on the markdown file

--- src/aula_036_ArraysMultidimensionais01/Aula35ArraysMultidimensionais.java
+++ src/aula_036_ArraysMultidimensionais01/Aula35ArraysMultidimensionais.java
@@ -10,9 +10,9 @@ public class Aula35ArraysMultidimensionais {
         dias[0][1] = 28;
         dias[0][2] = 31;
 
-        dias[1][0] = 31;
-        dias[1][1] = 28;
-        dias[1][2] = 31;
+        dias[1][0] = 30;
+        dias[1][1] = 31;
+        dias[1][2] = 30;
 
         for (int i = 0; i < dias.length; i++) {
             for (int j = 0; j < dias[0].length; j++) {
